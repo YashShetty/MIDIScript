60000493471eaff2754a94f61a500c90958554c7
Yash Shetty <yashetty388@gmail.com>
2020-05-09 23:15:18 +1000

    Security emails

--- overview/script.js
+++ overview/script.js
@@ -3,8 +3,8 @@
 
 // Write your Javascript code.
 
-var CLIENT_ID = '50416297127-ue1qmlr5dslasknpgk6uv80fqls6nbja.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyAJt_qH8cvqMmqe4diOMk0b4rxX-DHzZf8';
+var CLIENT_ID// = '50416297127-ue1qmlr5dslasknpgk6uv80fqls6nbja.apps.googleusercontent.com';
+var API_KEY;// = 'AIzaSyAJt_qH8cvqMmqe4diOMk0b4rxX-DHzZf8';
 var DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
 var SCOPES = 'https://www.googleapis.com/auth/drive';
 
